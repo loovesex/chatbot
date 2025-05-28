40dabb6afa96ca711840a54f6eb3218af8da21b8
lovesex <felipemmachad@gmail.com>
2025-05-28 00:48:40 -0300

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -18,7 +18,7 @@ from pathlib import Path
 # ======================
 class Config:
     # Configurações da API
-    API_KEY = "MEU_CEREBRO"
+    API_KEY = "AIzaSyB1Te3Iv5Tyi8DgG45pvuq9CjVjY-rV004"
     API_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={API_KEY}"
     
     # URLs de checkout/páginas
